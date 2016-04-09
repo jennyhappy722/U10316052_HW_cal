7bbdc4d5f3f8f0dbafa5b50e8dff318cb8fa57b7
jennyhappy722 <u10316052@go.utaipei.edu.tw>
2016-04-09 13:55:59 +0800

    Create Testcal.java

---
+++ Testcal.java
@@ -0,0 +1,26 @@
+
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.layout.Pane;
+import javafx.scene.paint.Color;
+import javafx.stage.Stage;
+import javafx.scene.shape.Line;
+import javafx.scene.layout.Pane;
+import javafx.scene.shape.Ellipse;
+import javafx.scene.*;
+
+public class Testcal extends Application{
+	@Override
+	public void start(Stage primaryStage){
+
+		Cal pane=new Cal();
+		Scene scene = new Scene(pane, 300, 450);
+    	primaryStage.setTitle("Cal"); // Set the stage title
+    	primaryStage.setScene(scene);
+    	primaryStage.show();		
+	}
+	
+        public static void main(String[] args){
+            Application.launch(args);
+        } 			
+}
